50efcd3f0993367ce7a4c69276a31432b7ee7b1b
Din XAR <131664922+KharunDima@users.noreply.github.com>
2023-07-25 17:57:14 +0300

    Update main.py

--- Test-Android-Chat-gpt/Test-GPT/main.py
+++ Test-Android-Chat-gpt/Test-GPT/main.py
@@ -1,8 +1,12 @@
+import sys
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.progressbar import ProgressBar
 from kivy.clock import Clock
 import asyncio
 from functools import partial
@@ -16,7 +20,7 @@ class ClientApp(App):
         layout.add_widget(question_label)
 
         self.message_input = TextInput()
-        self.message_input.hint_text = "Задайте свой вопрос:"  # Добавленная надпись
+        self.message_input.hint_text = "Задайте свой вопрос:"
         layout.add_widget(self.message_input)
 
         send_button = Button(text="ОТПРАВИТЬ", on_press=self.send_message_async,
@@ -26,7 +30,7 @@ class ClientApp(App):
         layout.add_widget(send_button)
 
         self.response_input = TextInput(readonly=True)
-        self.response_input.hint_text = "ChatGPT:"  # Добавленная надпись
+        self.response_input.hint_text = "ChatGPT:"
         layout.add_widget(self.response_input)
 
         return layout
@@ -36,9 +40,22 @@ class ClientApp(App):
         message = self.message_input.text
         self.message_input.text = ""
 
+        # Проверка на пустой текст сообщения
+        if not message.strip():
+            self.update_response("Ошибка: Пустой текст сообщения")
+            return
+
         # Здесь нужно указать IP-адрес и порт сервера
         server_ip = "94.241.40.84"
         server_port = 80
+
+        # Отображение модального окна с индикатором загрузки
+        loading_popup = Popup(title="ЗГРУЗКА", content=ProgressBar(max=10), size_hint=(0.3, 0.1),
+                              background_color=(0,0,0))
+        loading_popup.open()
+
+        # Отключение пользовательского ввода
+        Window.disabled = True
 
         # Обертка функции async в обычную функцию
         def send_message_sync(*args):
@@ -47,14 +64,22 @@ class ClientApp(App):
 
             # Обертка send_message_async для работы с новым event loop
             async def send_message_async_wrap():
-                reader, writer = await asyncio.open_connection(server_ip, server_port)
-                writer.write(message.encode())
-                await writer.drain()
+                try:
+                    reader, writer = await asyncio.open_connection(server_ip, server_port)
+                    writer.write(message.encode())
+                    await writer.drain()
 
-                response = await reader.read(1024)
-                self.update_response(response.decode())
+                    response = await reader.read(1024)
+                    self.update_response(response.decode())
 
-                writer.close()
+                    writer.close()
+
+                except OSError as e:
+                    self.update_response("Ошибка при подключении к серверу: " + str(e))
+
+                # Закрытие модального окна и включение пользовательского ввода
+                loading_popup.dismiss()
+                Window.disabled = False
 
             loop.run_until_complete(send_message_async_wrap())
             loop.close()
