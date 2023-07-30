505404ab408c2158b6dcda7e01f539b1f55a5146
Dmitriy <127970080+kyotohowrage@users.noreply.github.com>
2023-07-31 02:15:29 +0700

    Update main.py

--- Test-Android-Chat-gpt/Test-GPT/main.py
+++ Test-Android-Chat-gpt/Test-GPT/main.py
@@ -11,89 +11,50 @@ import asyncio
 from functools import partial
 
 
-
 class Container(GridLayout):
-    
-    
-    #  значения идут из сlient.kv
-    # values are going from сlient.kv
+    server_ip = "94.241.40.84"
+    server_port = 80
+
     label_w = ObjectProperty()
     text_input_w = ObjectProperty()
     button_send_w = ObjectProperty()
-    # text_input_readonly_w = ObjectProperty()
 
-
-
-   
-
-    
-    
-
-     # checking for empty message
     def on_text_change(self, text):
-        if len(text.strip()) == 0 or text.isspace():
-            self.button_send_w.disabled = True
-        else:
-            self.button_send_w.disabled = False
+        self.button_send_w.disabled = not text.strip()
 
-
-    # func for send message to server
     def send_message_async(self, *args):
-
-
         message = self.text_input_w.text
         self.text_input_w.text = ""
 
-         # Проверка на пустой текст сообщения
         if not message.strip():
             self.update_response("Ошибка: Пустой текст сообщения")
             return
 
-        # Здесь нужно указать IP-адрес и порт сервера
-        server_ip = "94.241.40.84"
-        server_port = 80
-
-        # Отображение модального окна с индикатором загрузки
         loading_popup = Popup(title="ЗАГРУЗКА", content=ProgressBar(max=10), size_hint=(0.3, 0.1),
-                              background_color=(0,0,0))
+                              background_color=(0, 0, 0))
         loading_popup.open()
 
-        # Отключение пользовательского ввода
         Window.disabled = True
 
-        # Обертка функции async в обычную функцию
-        def send_message_sync(*args):
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
+        async def send_message_async_wrap():
+            try:
+                reader, writer = await asyncio.open_connection(self.server_ip, self.server_port)
+                writer.write(message.encode())
+                await writer.drain()
+                response = await reader.read(4096)
+                self.update_response(response.decode())
+                writer.close()
 
-            # Обертка send_message_async для работы с новым event loop
-            async def send_message_async_wrap():
-                try:
-                    reader, writer = await asyncio.open_connection(server_ip, server_port)
-                    writer.write(message.encode())
-                    await writer.drain()
-                    #Размер буфера сообщения
-                    response = await reader.read(4096)
-                    self.update_response(response.decode())
-                    
-                    writer.close()
+            except OSError as e:
+                self.update_response("Ошибка при подключении к серверу: " + str(e))
 
-                except OSError as e:
-                    self.update_response("Ошибка при подключении к серверу: " + str(e))
+            loading_popup.dismiss()
+            Window.disabled = False
 
-                # Закрытие модального окна и включение пользовательского ввода
-                loading_popup.dismiss()
-                Window.disabled = False
-
-            loop.run_until_complete(send_message_async_wrap())
-            loop.close()
-
-        # Запуск обернутой функции async через Clock
-        Clock.schedule_once(partial(send_message_sync), 0)
+        asyncio.create_task(send_message_async_wrap())
 
     def update_response(self, response):
         self.label_w.text = response
-        
 
 
 class ClientApp(MDApp):
@@ -102,5 +63,5 @@ class ClientApp(MDApp):
 
 
 if __name__ == "__main__":
-    Window.clearcolor = (1, 1, 1, 1)  # Цвет фона окна
+    Window.clearcolor = (1, 1, 1, 1)
     ClientApp().run()
