da80a1b05e764f65f991614001862e4b5ebe491d
Dmitriy <127970080+kyotohowrage@users.noreply.github.com>
2023-07-31 02:16:20 +0700

    Update server.py

--- Test-Android-Chat-gpt/Test-GPT/server.py
+++ Test-Android-Chat-gpt/Test-GPT/server.py
@@ -8,14 +8,14 @@ openai.api_key = "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"
 
 async def handle_client(reader, writer):
     try:
-        message = (await reader.read(1024)).decode()
+        message = (await reader.read(2048)).decode()
         addr = writer.get_extra_info('peername')
         print(f"Сообщение от клиента {addr}: {message}")
 
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=message,
-            max_tokens=1024
+            max_tokens=2048
         )
 
         model_response = response.choices[0].text.strip()
