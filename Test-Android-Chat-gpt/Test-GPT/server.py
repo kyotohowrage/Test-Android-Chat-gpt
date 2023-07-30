ec001c12d8dbf3271cf2b650425cd9dd4475b150
Dmitriy <127970080+kyotohowrage@users.noreply.github.com>
2023-07-31 02:15:50 +0700

    Update server.py

--- Test-Android-Chat-gpt/Test-GPT/server.py
+++ Test-Android-Chat-gpt/Test-GPT/server.py
@@ -1,45 +1,38 @@
 import asyncio
 import openai
 
-# Здесь вы должны указать IP-адрес сервера и порт для прослушивания
 server_ip = "192.168.1.106"
 server_port = 1234
 
 openai.api_key = "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"
 
 async def handle_client(reader, writer):
-    # Получить сообщение от клиента
-    message = (await reader.read(1024)).decode()
-    addr = writer.get_extra_info('peername')
-    print(f"Сообщение от клиента {addr}: {message}")
+    try:
+        message = (await reader.read(1024)).decode()
+        addr = writer.get_extra_info('peername')
+        print(f"Сообщение от клиента {addr}: {message}")
 
-    # Отправить сообщение в OpenAI
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=message,
-        max_tokens=1024
-    )
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=message,
+            max_tokens=1024
+        )
 
-    # Получить ответ от OpenAI
-    model_response = response.choices[0].text.strip()
-    print("Ответ от OpenAI:", model_response)
+        model_response = response.choices[0].text.strip()
+        print("Ответ от OpenAI:", model_response)
 
-    # Отправить ответ клиенту
-    writer.write(model_response.encode())
-    await writer.drain()
+        writer.write(model_response.encode())
+        await writer.drain()
 
-    writer.close()
-
+    finally:
+        writer.close()
 
 async def start_server():
-    server = await asyncio.start_server(
-        handle_client, server_ip, server_port
-    )
+    server = await asyncio.start_server(handle_client, server_ip, server_port)
     print("Сервер слушает на {}:{}".format(server_ip, server_port))
 
     async with server:
         await server.serve_forever()
-
 
 if __name__ == "__main__":
     asyncio.run(start_server())
